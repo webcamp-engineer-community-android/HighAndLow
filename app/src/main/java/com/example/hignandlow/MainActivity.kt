2d6d3feca1114bea887506219d8659995524f321
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2021-05-22 10:55:01 +0900

    自身のカードをランダム生成して表示

--- app/src/main/java/com/example/hignandlow/MainActivity.kt
+++ app/src/main/java/com/example/hignandlow/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.hignandlow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -26,5 +27,34 @@ class MainActivity : AppCompatActivity() {
         hitText.text = getString(R.string.hit_text)
         loseText.text = getString(R.string.lose_text)
         gameStart = true
+        drawCard()
+    }
+
+    private fun drawCard() {
+        // カード表示
+        yourCardImage.setImageResource(R.drawable.z02)
+        droidCardImage.setImageResource(R.drawable.z01)
+
+        // 乱数生成
+        yourCard = (1..13).random()
+        Log.d(tag, "You:" + yourCard)
+        when (yourCard) {
+            1 -> yourCardImage.setImageResource(R.drawable.d01)
+            2 -> yourCardImage.setImageResource(R.drawable.d02)
+            3 -> yourCardImage.setImageResource(R.drawable.d03)
+            4 -> yourCardImage.setImageResource(R.drawable.d04)
+            5 -> yourCardImage.setImageResource(R.drawable.d05)
+            6 -> yourCardImage.setImageResource(R.drawable.d06)
+            7 -> yourCardImage.setImageResource(R.drawable.d07)
+            8 -> yourCardImage.setImageResource(R.drawable.d08)
+            9 -> yourCardImage.setImageResource(R.drawable.d09)
+            10 -> yourCardImage.setImageResource(R.drawable.d10)
+            11 -> yourCardImage.setImageResource(R.drawable.d11)
+            12 -> yourCardImage.setImageResource(R.drawable.d12)
+            13 -> yourCardImage.setImageResource(R.drawable.d13)
+        }
+        droidCard = (1..13).random()
+        Log.d(tag, "droid:" + droidCard)
+        answerd = false
     }
 }
