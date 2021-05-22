87c10080e0f402280db5066b8a0512b16e5a73cb
NoriakiOtsuka <carly13rae7jepson@gmail.com>
2021-05-22 10:35:29 +0900

    変数宣言

--- app/src/main/java/com/example/hignandlow/MainActivity.kt
+++ app/src/main/java/com/example/hignandlow/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.hignandlow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val tag = "high and low"
+    private var yourCard = 0
+    private var droidCard = 0
+    private var hitCount = 0
+    private var loseCount = 0
+    private var gameStart = false
+    private var answerd = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        hitCount = 0
+        loseCount = 0
+        hitText.text = getString(R.string.hit_text)
+        loseText.text = getString(R.string.lose_text)
+        gameStart = true
     }
 }
